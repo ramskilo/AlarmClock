1ee11454721ac44e7cad14cc4cdcf3608584de5c
Ramskilo <ramskilo@gmail.com>
2024-03-30 01:04:48 +0100

    Writing of the played file to a file to not repeat the same song too often

--- AlarmClock.py
+++ AlarmClock.py
@@ -5,8 +5,8 @@ import string
 import random
 import glob
 import json
-
-from signal import signal, SIGINT
+import keyboard
+from signal import signal, SIGINT, SIGCONT
 from sys import exit
 
 def handler(signal_received, frame):
@@ -14,49 +14,74 @@ def handler(signal_received, frame):
     print('SIGINT or CTRL-C detected. Exiting gracefully')
     exit(0)
 
+def handlerNext(signal_received, frame):
+    # Handle any cleanup here
+    print('Next')
+    exit(0)
+
+def load_json_data(json_data):
+    try:
+        # Attempt to decode the JSON data
+        data = json.load(json_data)
+        return data
+    except json.JSONDecodeError:
+        # Handle the JSONDecodeError by printing an error message
+        return  {"songsPlayed": []}
 
 if __name__ == '__main__':
-    # Tell Python to run the handler() function when SIGINT is recieved
+    # Tell Python to run the handler() function when SIGINT is received
     signal(SIGINT, handler)
+    signal(SIGCONT, handlerNext)
+
     l_song_to_play = ""
-    l_song = []
     l_times = 1
-    timeout = 60 #seconds
-    
-    f = open('settings.json')
-    data = json.load(f)
-    
-    l_number_of_songs = data['NumberOfSongsToPlay']
-    l_songs_directory = data['SongsDirectory']
-    l_extension = data['MediaFileExtension']
-    
-    alarmHour = data['DefaultHour']
-    alarmMin = data['DefaultMinutes']
-    
-    if alarmHour is None  or alarmMin is None:  
-        alarmHour = int(input("Which time do you wanna wake up? (Hour)"))
-        alarmMin = int(input("minutes:")) 
-    
+    timeout = 60  # seconds
+    played = {"songsPlayed": []}
+
+    with open('settings.json') as f:
+        data = load_json_data(f)
+
+    with open('played.json') as f:
+        played = load_json_data(f)
+
+    l_number_of_songs = data.get('NumberOfSongsToPlay', 0)
+    l_songs_directory = data.get('SongsDirectory', '')
+    l_extension = data.get('MediaFileExtension', '')
+
+    alarmHour = data.get('DefaultHour')
+    alarmMin = data.get('DefaultMinutes')
+
+    if alarmHour is None or alarmMin is None:
+        alarmHour = int(input("What time do you want to wake up? (Hour)"))
+        alarmMin = int(input("Minutes:"))
+
+    print(f"{alarmHour}:{alarmMin}")
     print('Running. Press CTRL-C to exit.')
+
     while True:
         time.sleep(0.2)
-        if alarmHour==datetime.datetime.now().hour and alarmMin==datetime.datetime.now().minute or ( l_times <= l_number_of_songs and l_times > 1 ):
+        current_time = datetime.datetime.now()
+        if (alarmHour == current_time.hour and alarmMin == current_time.minute) or (l_times <= l_number_of_songs and l_times > 1):
             print("Playing...")
-            while l_song_to_play == "":
+            while not l_song_to_play:
                 start = time.time()
-                l_letter = random.choice(string.ascii_letters + string.digits)
+                l_letter = random.choice('LM' + string.digits)
                 l_letter2 = random.choice(string.ascii_letters)
-                l_glob = l_songs_directory + l_letter2 + "*/" + l_letter + l_extension
-                l_song = glob.glob(l_glob) 
-                if len(l_song) > 0:
+                l_constr_pattern = f"{l_songs_directory}{l_letter2}*/{l_letter}{l_extension}"
+                l_song = glob.glob(l_constr_pattern)
+                if l_song:
                     l_song_to_play = l_song[0]
                 if time.time() - start >= timeout:
                     break
+                if l_song_to_play in played.get("songsPlayed", []):
+                    l_song_to_play = ""
             print(l_song_to_play)
-            if not len(l_song_to_play) == 0:
+            if l_song_to_play:
+                played.get("songsPlayed").append(l_song_to_play)
+                with open("played.json", 'w') as filehandle:
+                    json.dump(played, filehandle)
                 playsound(l_song_to_play)
                 l_song_to_play = ""
                 l_times += 1
             if l_times > l_number_of_songs:
                 break
-        
