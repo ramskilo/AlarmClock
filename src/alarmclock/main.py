5de6ef239e96b3ac7ee63d736247224af30657d5
Luca Dario Carità <lucadariocarita@gmail.com>
2024-09-11 22:17:30 +0200

    Tkinter keypress bind

--- src/alarmclock/main.py
+++ src/alarmclock/main.py
@@ -7,7 +7,6 @@ import settings
 root = tkinter.Tk()
 root.title("Alarm Clock")
 root.geometry("600x400")
-root.mainloop()
 
 alarm_is_on = True
 
@@ -17,21 +16,27 @@ def find_files(dirfiles, extension):
     return list(path.rglob("*.{}".format(extension)))
 
 
+def on_key_press(event):
+    if event.keysym == "Escape":
+        print("Escape key pressed, exiting...")
+        root.quit()
+    else:
+        print(f"Key pressed: {event.keysym}")
+
+
 music_files = find_files(settings.SONGS_DIRECTORY, settings.MEDIA_FILE_EXTENSION)
 print(f"{len(music_files)} files found")
 for mp3 in music_files:
     print(mp3)
 
-while alarm_is_on:
-    pass
-    # for event in pygame.event.get():
-    #     if event.type == pygame.QUIT:  # exit the program
-    #         alarm_is_on = False
-    #     if event.type == pygame.KEYDOWN:  # key pressed
-    #         if event.key == pygame.K_LEFT:  # left arrow key
-    #             print("Previous")
-    #         if event.key == pygame.K_RIGHT:  # right arrow key
-    #             print("Next")
-    # pygame.display.flip()
-
-sys.exit()
+root.bind("<KeyPress>", on_key_press)
+root.mainloop()
+# for event in pygame.event.get():
+#     if event.type == pygame.QUIT:  # exit the program
+#         alarm_is_on = False
+#     if event.type == pygame.KEYDOWN:  # key pressed
+#         if event.key == pygame.K_LEFT:  # left arrow key
+#             print("Previous")
+#         if event.key == pygame.K_RIGHT:  # right arrow key
+#             print("Next")
+# pygame.display.flip()
