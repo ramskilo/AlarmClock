115a89f8a72451f403463792e227a3775caa2e83
Luca Dario Carità <lucadariocarita@gmail.com>
2024-09-12 16:36:42 +0200

    Debounce + Player (interface only)

--- src/alarmclock/main.py
+++ src/alarmclock/main.py
@@ -1,14 +1,43 @@
-import sys
+import time
 import tkinter
 from pathlib import Path
 
 import settings
 
-root = tkinter.Tk()
-root.title("Alarm Clock")
-root.geometry("600x400")
 
-alarm_is_on = True
+class Debounce:
+    def __init__(self, root):
+        self.root = root
+        self.last_trigger_time = 0
+        self.debounce_delay = settings.DEBOUNCE_DELAY
+        self.root.bind("<Key>", self.on_key_press)
+
+    def on_key_press(self, event):
+        current_time = time.time() * 1000
+        if current_time - self.last_trigger_time > self.debounce_delay:
+            self.last_trigger_time = current_time
+            process_key(event)
+
+
+class Radio:
+    def __init__(self, playlist):
+        self.playlist = playlist
+        self.current = 0
+
+    def play(self):
+        print("Playing song")
+
+    def stop(self):
+        print("Stopping song")
+
+    def next(self):
+        print("Next song")
+
+    def previous(self):
+        print("Previous song")
+
+    def current_song(self):
+        return self.playlist[self.current]
 
 
 def find_files(dirfiles, extension):
@@ -16,27 +45,35 @@ def find_files(dirfiles, extension):
     return list(path.rglob("*.{}".format(extension)))
 
 
-def on_key_press(event):
+def process_key(event):
     if event.keysym == "Escape":
-        print("Escape key pressed, exiting...")
-        root.quit()
+        app.quit()
     else:
         print(f"Key pressed: {event.keysym}")
+        if event.keysym == "Left":
+            radio.previous()
+        if event.keysym == "Right":
+            radio.next()
 
 
-music_files = find_files(settings.SONGS_DIRECTORY, settings.MEDIA_FILE_EXTENSION)
-print(f"{len(music_files)} files found")
-for mp3 in music_files:
+def main():
+    print("Starting Radio!")
+    # root.after(1000, say_hello)
+
+
+# Load songs
+filelist = find_files(settings.SONGS_DIRECTORY, settings.MEDIA_FILE_EXTENSION)
+for mp3 in filelist:
     print(mp3)
 
-root.bind("<KeyPress>", on_key_press)
-root.mainloop()
-# for event in pygame.event.get():
-#     if event.type == pygame.QUIT:  # exit the program
-#         alarm_is_on = False
-#     if event.type == pygame.KEYDOWN:  # key pressed
-#         if event.key == pygame.K_LEFT:  # left arrow key
-#             print("Previous")
-#         if event.key == pygame.K_RIGHT:  # right arrow key
-#             print("Next")
-# pygame.display.flip()
+# Start GUI
+app = tkinter.Tk()
+app.title("Alarm Clock")
+app.geometry("600x400")
+# Start radio istance
+radio = Radio(playlist=filelist)
+# Debounce click events
+debounce = Debounce(app)
+# Start main loop
+app.after(200, main)
+app.mainloop()
--- src/alarmclock/settings.py
+++ src/alarmclock/settings.py
@@ -7,3 +7,4 @@ load_dotenv()
 SONGS_DIRECTORY = os.getenv("SONGS_DIRECTORY", "./songs")
 MEDIA_FILE_EXTENSION = os.getenv("MEDIA_FILE_EXTENSION", "mp3")
 NUMBER_OF_SONGS_TO_PLAY = 1
+DEBOUNCE_DELAY = 1500  # milliseconds
